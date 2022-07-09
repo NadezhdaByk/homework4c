e39f4b6c1a073e7ac60bddc4a13715f8846d7b7e
Nadezhda <nbykova79@gmail.com>
2022-07-09 22:02:44 +0300

    сохранили код программы

---
+++ TASK27/Program.cs
@@ -0,0 +1,20 @@
+﻿//принимает на вход число и выдает суммы цифр в числе
+Console.WriteLine("Введите число");
+string num1 = Console.ReadLine();
+int num = Convert.ToInt32(num1);
+int Funct()
+{
+    int N = num1.Length;
+    var array = num.ToString().Select(e=>int.Parse(e.ToString())).ToArray();
+    //int[] array = new int[num.Length];
+    //char [] array = num.ToCharArray(); 
+    int sum = 0;
+
+    for (int i=0; i<N; i++)
+    {
+        sum = sum + array[i];
+    }
+return sum;
+
+}
+Console.WriteLine(Funct());
